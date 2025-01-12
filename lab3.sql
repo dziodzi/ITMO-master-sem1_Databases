75da669262578a8948991e38cf6495710979fac6
Gregory <79766495+dziodzi@users.noreply.github.com>
2025-01-12 14:29:44 +0300

    add lab3.sql

---
+++ lab3.sql
@@ -0,0 +1,9 @@
+# Вывести на экран названия категорий, количество подкатегорий и количество товаров в них
+
+SELECT PC.Name, COUNT(DISTINCT P.ProductSubcategoryID), COUNT(ProductID)
+FROM Production.Product AS P
+JOIN Production.ProductSubcategory AS PSC 
+  ON P.ProductSubcategoryID = PSC.ProductSubcategoryID
+JOIN Production.ProductCategory AS PC
+  ON PC.ProductCategoryID = PSC.ProductCategoryID
+GROUP BY PC.Name
